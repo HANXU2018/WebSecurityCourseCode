1647cd0c1cedd2ba015851f36a5004197a421413
HANXU <1076998404@qq.com>
2020-05-16 22:31:34 +0800

    session登录拦截

---
+++ src/main/java/com/safe/web1/config/MyWebConfigurer.java
@@ -0,0 +1,21 @@
+package com.safe.web1.config;
+
+import com.safe.web1.interceptor.LoginInterceptor;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.*;
+
+@SpringBootConfiguration
+public class MyWebConfigurer implements WebMvcConfigurer {
+
+    @Bean
+    public LoginInterceptor getLoginIntercepter() {
+        return new LoginInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(getLoginIntercepter()).addPathPatterns("/**").excludePathPatterns("/index.html");
+    }
+}
+
