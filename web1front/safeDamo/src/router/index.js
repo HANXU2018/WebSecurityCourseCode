87098fcac8b7a639a2e48f1092e1fe783f7a52e9
HANXU <1076998404@qq.com>
2020-05-16 10:21:07 +0800

    前端login页面引入

--- web1front/safeDamo/src/router/index.js
+++ web1front/safeDamo/src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import Login from '@/components/Login'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
-      path: '/',
+      path: '/RSA',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/Login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
